1d641efb0e55c4c89445ead173b31674a7fe5d45
Akshay S Dinesh <asdofindia@gmail.com>
2016-03-15 05:19:59 +0530

    add malayalam, hindi, kannada

--- test/all.js
+++ test/all.js
@@ -4,8 +4,23 @@ import {decode} from 'project/katapayadi'
 chai.should();
 
 describe('katapayadi', () => {
-    it('should decode properly', () => {
-        decode("ശശി").should.equal(11);
-        decode("ചണ്ഡാംശുചന്ദ്രാധമകുംഭിപാലൈര്‍").should.equal(31415926536);
-    });
+  it('should decode malayalam properly', () => {
+    decode('ശശി').should.equal('55');
+    decode('ചണ്ഡാംശുചന്ദ്രാധമകുംഭിപാലൈര്‍').should.equal('31415926536');
+    decode('കാക്ക').should.equal('11');
+    decode('ആയുരാരോഗ്യസൌഖ്യം').should.equal('1712210');
+  });
+  it('should decode hindi properly', () => {
+    // decode(` गोपीभाग्यमधुव्रात-श्रुग्ङिशोदधिसन्धिग ॥
+ // खलजीवितखाताव गलहालारसंधर ॥`).should.equal('31415926535897932384626433832792');
+    decode('आयुरारोग्यसौख्यम्').should.equal('1712210');
+  });
+  it('should decode kannada properly', () => {
+    decode(`ಗೋಪೀಭಾಗ್ಯಮಧುವ್ರಾತ-ಶ್ರುಂಗಿಶೋದಧಿಸಂಧಿಗ ||
+ ಖಲಜೀವಿತಖಾತಾವ ಗಲಹಾಲಾರಸಂಧರ ||`).should.equal('31415926535897932384626433832792');
+  });
+  it('should decode telugu properly', () => {
+    decode(`గోపీభాగ్యమధువ్రాత-శృంగిశోదధిసంధిగ |
+ ఖలజీవితఖాతావ గలహాలారసంధర ||`).should.equal('31415926535897932384626433832792');
+  });
 });
